140e31a43d7aae201da64976b455d520716d7c60
Wyglett Luirrare Albuquerque dos Santos de Paula <wyglett.fapes@gmail.com>
2026-03-03 15:22:41 +0000

    File changes

--- src/components/acompanhamento/GastosFinanceiro.jsx
+++ src/components/acompanhamento/GastosFinanceiro.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
